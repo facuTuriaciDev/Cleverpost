1924ee9bef067ec93ee319f2cb0ee00de6bb4ffe
Facu <bestjayceeu@gmail.com>
2023-04-12 13:04:32 +0200

    Redux store and postSlice

---
+++ src/slices/posts/postSlice.ts
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+  return response.data;
+});
+
+interface PostState {
+  posts: Post[];
+  status: 'idle' | 'loading' | 'failed';
+  error: string | null;
+}
+
+const initialState: PostState = {
+  posts: [],
+  status: 'idle',
+  error: null
+};
+
+const postSlice = createSlice({
+  name: 'post',
+  initialState,
+  reducers: {
+    //TODO addPost, deletePost, editPost
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPosts.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.posts = action.payload;
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message || null;
+      });
+  }
+});
+
+export default postSlice.reducer;
